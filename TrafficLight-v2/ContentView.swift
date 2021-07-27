934ca1189aa893608aca18ea433d5c26dbc1b875
Oleg Tsarenkoff <tsarenkow@icloud.com>
2021-07-27 16:49:29 +0300

    Initial Commit

---
+++ TrafficLight-v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLight-v2
+//
+//  Created by Oleg Tsarenkoff on 27.07.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLight-v2/TrafficLight_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLight_v2App.swift
+//  TrafficLight-v2
+//
+//  Created by Oleg Tsarenkoff on 27.07.21.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLight_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
